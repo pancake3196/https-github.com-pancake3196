9d1df9d45f5764bd932f695101e7828f2ed1f39a
pancake3196 <104414194+pancake3196@users.noreply.github.com>
2023-11-17 11:01:10 +0900

    [level unrated] Title: 길이에 따른 연산, Time: 0.02 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.java
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -6,18 +6,22 @@ class Solution {
         for(int i = 0; i < num_list.length; i++){   //num_list의 길이만큼 i를 1씩 더하다가
            
             if(num_list.length >= 11){              //num_list의 길이가 11 이상이면
-          fle += num_list[i];                    //ans에 num_list의 i번째 주소값을 더함
+          fle += num_list[i];                    //int형의 fle에 num_list의 i번째 주소값을 더함
+                answer = fle;                   //전부다 더한 값을 answer에 넣음
                 
-                 answer = fle;                        
         }else {                                   //만약 i가 k이하면
-          gop *= num_list[i];                    //answer에 num_list의 i번째 주소값을 곱함
-        answer = gop;
+          gop *= num_list[i];                    //int형 주소값에 num_list의 i번째 주소값을 곱함
+        answer = gop;                            //전부다 곱한 값을 answer에 넣음
+                
+                //fle이 0인 이유 : 1로 하면 결과값+1로 출력되기 때문
+                //fop이 1인 이유 : 0으로 하면 0*n이 되서 무슨 값을 넣든 0이되서
+                
         }
           
             
         }
        
-       return answer;
+       return answer;   //fle,gop을 answer에 넣었으니 answer를 리턴한다
     }
             
 
